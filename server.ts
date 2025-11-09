f14f74a70f64f4ae1af6d03890d95631d9fbf47a
Jackson Clawson <jclaw3456@gmail.com>
2025-11-09 16:16:53 -0500

    clean up

--- server.ts
+++ server.ts
@@ -1,17 +1,12 @@
-//Import express
 import express from 'express';
 import socket from 'socket.io';
 
-// Create the Express app
 const app = express();
 
-// Set the port used for server traffic.
 const port = 3000;
 
-// Middleware to serve static files from 'dist' directory
 app.use(express.static('dist'));
 
-//Run server at port
 const server = app.listen(port);
 
 console.log(`Server running at http://localhost:${port}`);
@@ -34,7 +29,6 @@ io.sockets.on('connection', (socket) => {
   });
 
   function mouseMessage(data: { x: number; y: number }) {
-    // Broadcast the mouse data to all other clients
     socket.broadcast.emit('mouse', data);
     console.log('Mouse data received:', data);
   }
